845dd2ba35b80c08f6ef054871e72cc5f2562ed8
Eric Winkler <winkler.em@gmail.com>
2014-10-07 17:09:37 -0400

    Added package dependency install script

---
+++ scripts/install_packages.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+################################################################################
+# For Ubuntu systems
+# installs package dependencies
+################################################################################
+apt-get install libapache2-mod-wsgi \
+                libapache2-mod-jk \
+                python-cjson \
+                python-django \
+                python-django-piston \
+                python-mysqldb \
+                python-libxml2 \
+                python-lxml \
+                sqlite3
